82a4a6abe2a4770195e834ac48844c3b231aba17
toshi <nakamura@infr.red>
2025-01-14 12:17:04 +0900

    change code

---
+++ script.js
@@ -0,0 +1,17 @@
+const playButton = document.getElementById('play-button');
+
+playButton.addEventListener('click', () => {
+    const sounds = [
+        'sounds/laugh1.mp3',
+        'sounds/laugh2.mp3',
+        'sounds/laugh3.mp3',
+        'sounds/laugh4.mp3',
+        'sounds/laugh5.mp3',
+        'sounds/laugh6.mp3',
+        'sounds/laugh7.mp3',
+        'sounds/laugh8.mp3'
+    ];
+    const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
+    const audio = new Audio(randomSound);
+    audio.play();
+});
